6731345fa0125aeaa4bf39b980da524ed59628d8
xGanczix <95712763+xGanczix@users.noreply.github.com>
2022-07-12 20:11:33 +0200

    Add model for normal users
    normal users can't see ContactNumber from Cinema table

---
+++ Models/CinemaClient.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace CinemasAPI.Models
+{
+    public class CinemaClient
+    {
+        public string Name { get; set; }
+        public bool IsOpen { get; set; }
+        public string ContactEmail { get; set; }
+        public string City { get; set; }
+        public string Street { get; set; }
+        public string PostalCode { get; set; }
+        public List<FilmClient> Films { get; set; }
+    }
+}
---
+++ Models/FilmClient.cs
@@ -0,0 +1,11 @@
+﻿namespace CinemasAPI.Models
+{
+    public class FilmClient
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Minutes { get; set; }
+        public decimal Price { get; set; }
+    }
+}
