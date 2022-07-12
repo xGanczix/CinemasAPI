0d9986cf259ba6af3455fc10e982513a4e9c346b
xGanczix <95712763+xGanczix@users.noreply.github.com>
2022-07-12 18:57:32 +0200

    Configure connection to database

---
+++ Entities/CinemaDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CinemasAPI.Entities
+{
+    public class CinemaDbContext : DbContext
+    {
+        private string _connectionString = "Server=localhost\\sqlexpress;Database=CinemaAPI;Trusted_Connection=True";
+        public DbSet<Cinema> Cinemas { get; set; }
+        public DbSet<Address> Addreses { get; set; }
+        public DbSet<Film> Films { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_connectionString);
+        }
+    }
+}
