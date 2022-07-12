137d731b40e1acc76e951b0e890fd6559e0326d5
xGanczix <95712763+xGanczix@users.noreply.github.com>
2022-07-12 20:06:23 +0200

    Add method GET and set default start page

---
+++ Controllers/CinemaController.cs
@@ -0,0 +1,29 @@
+﻿using CinemasAPI.Entities;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CinemasAPI.Controllers
+{
+    [Route("api/cinema")]
+    public class CinemaController : ControllerBase
+    {
+        private readonly CinemaDbContext _dbContext;
+        public CinemaController(CinemaDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Cinema>> GetAll()
+        {
+            var cinemas = _dbContext
+                .Cinemas
+                .ToList();
+
+            return Ok(cinemas); 
+        }
+
+        
+    }
+}
