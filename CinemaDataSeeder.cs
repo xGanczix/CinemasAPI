64aaaa5eafb3ff986694d005c1978482a4b68b7a
xGanczix <95712763+xGanczix@users.noreply.github.com>
2022-07-13 19:37:41 +0200

    Add data to role table

--- CinemaDataSeeder.cs
+++ CinemaDataSeeder.cs
@@ -21,7 +21,37 @@ namespace CinemaAPI
                     _dbContext.Cinemas.AddRange(cinemas);
                     _dbContext.SaveChanges();
                 }
+
+                if(!_dbContext.Roles.Any())
+                {
+                    var roles = GetRoles();
+                    _dbContext.Roles.AddRange(roles);
+                    _dbContext.SaveChanges();
+                }
             }
+        }
+
+        private IEnumerable<Role> GetRoles()
+        {
+            var roles = new List<Role>()
+            {
+                new Role()
+                {
+                    Name = "User"
+                },
+
+                new Role()
+                {
+                    Name = "Manager"
+                },
+
+                new Role()
+                {
+                    Name = "Admin"
+                },
+            };
+
+            return roles;
         }
 
         private IEnumerable<Cinema> GetCinemas()
